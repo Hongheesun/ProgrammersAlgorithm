833876dc3e9d9dc351672cfca79d49149142c65b
Hia <91399033+Hongheesun@users.noreply.github.com>
2022-09-03 19:43:44 +0900

    Update n^2 배열 자르기.js

--- n^2 배열 자르기.js
+++ n^2 배열 자르기.js
@@ -1,14 +1,11 @@
 //https://velog.io/@developyoun/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-n2-%EB%B0%B0%EC%97%B4-%EC%9E%90%EB%A5%B4%EA%B8%B0-JS
 function solution(n, left, right) {
-  const answer = [];
+  const answre = [];
 
-  for (let i = left; i <= right; i++) {
-    // 7/4 = 1 = share
-    // 7%4 = 3 = reminder
-    const share = parseInt(i / n); // 행축(y)
-    const reminder = i % n; // 열축(x)
-    answer.push(Math.max(share, reminder) + 1);
+  for (let i = 0; i <= right; i++) {
+    const w = i / n;
+    const h = i % n;
+
+    answre.push(Math.max(w, h) + 1);
   }
-
-  return answer;
 }
